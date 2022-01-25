d7af4bfc7e84981d52c77d930c213b2975632a5f
beygee <doug0476@naver.com>
2022-01-25 23:34:39 +0900

    init

---
+++ data/headerNavLinks.ts
@@ -0,0 +1,8 @@
+const headerNavLinks = [
+  { href: '/blog', title: '블로그' },
+  { href: '/tags', title: '태그' },
+  // { href: '/projects', title: '프로젝트' },
+  { href: '/about', title: '정보' },
+]
+
+export default headerNavLinks
